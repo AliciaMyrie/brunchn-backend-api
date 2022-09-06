b9ce4005cd1cac9efeca3945094464667338f65a
Alicia Myrie <aliciamyrie@gmail.com>
2022-09-06 17:38:31 -0400

    added patch

--- functions/index.js
+++ functions/index.js
@@ -3,27 +3,21 @@
 import functions from 'firebase-functions';
 import express from 'express';
 import cors from 'cors';
-import { addRestaurants, getRestaurants } from "./src/restaurants.js"
+import { addRestaurants, getRestaurants, addLike  } from "./src/restaurants.js"
 
-// import dbconnect from './src/dbconect.js';
 
-// const client = new MongoClient(uri)
-
-// const collection = dbconnect()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-//  app.listen(4040, () => console.log("api listening on port 4040"))
-//POST
-// DELETE
-// PUT
-// app.get('/restaurants', getRestaurants);
 
 app.get("/getrestaurants", getRestaurants);
 
 app.post("/addrestaurants", addRestaurants);
+
+app.patch("/addlike/:id", addLike)
+
 
 
 app.get("/getall", (req, res) =>
--- functions/src/restaurants.js
+++ functions/src/restaurants.js
@@ -1,19 +1,31 @@
-import { MongoClient } from "mongodb";
-import { uri } from "./credentials.js"
+import { MongoClient } from 'mongodb';
+import { uri } from './credentials.js';
+import { ObjectId } from "mongodb";
 
-const client = new MongoClient (uri)
-const database = client.db("Brunch_n")
-const restaurantCollection = database.collection("Restaurants")
-console.log("Mongo Connected")
+const client = new MongoClient(uri);
+const database = client.db('Brunch_n');
+const restaurantCollection = database.collection('Restaurants');
+console.log('Mongo Connected');
 
 export const getRestaurants = (req, res) => {
-    restaurantCollection.find({}).toArray((err, restaurants) => {
-        res.send(restaurants)
-    });
-}
+  restaurantCollection.find({}).toArray((err, restaurants) => {
+    res.send(restaurants);
+  });
+};
 
 export const addRestaurants = async (req, res) => {
-    let newRestaurant = req.body;
-    await restaurantCollection.insertOne(newRestaurant);
-        res.send({ "Restaurant was added": true });
-      }
+  let newRestaurant = req.body;
+  await restaurantCollection.insertOne(newRestaurant);
+  res.send({ 'Restaurant was added': true });
+};
+
+
+// export const addLike = async (req, res) => {
+//     const { likes } = req.body
+//    const result = await restaurantCollection.findOneAndUpdate(
+//     {_id: new ObjectId (req.params.id)},
+//     { $inc: { Likes: likes }})
+//   res.send({ result });
+// };
+
+
