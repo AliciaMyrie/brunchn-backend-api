9f1608c4838bc7df8162430d5b8d354f95a8d6a0
Alicia Myrie <aliciamyrie@gmail.com>
2022-09-12 23:44:03 -0400

    fixed get route

--- functions/src/restaurants.js
+++ functions/src/restaurants.js
@@ -2,25 +2,34 @@ import { MongoClient } from 'mongodb';
 import { uri } from './credentials.js';
 import { ObjectId } from 'mongodb';
 
-const client = new MongoClient(uri);
-const database = client.db('Brunch_n');
-const restaurantCollection = database.collection('Restaurants');
-console.log('Mongo Connected');
+async function connectToMongo() {
+  const client = new MongoClient(uri);
+  const database = client.db('Brunch_n');
+  console.log('Mongo Connected');
+  return database.collection('Restaurants');
+}
 
-export const getRestaurants = (req, res) => {
+export const getRestaurants = async (req, res) => {
+  const restaurantCollection = await connectToMongo();
   restaurantCollection.find({}).toArray((err, restaurants) => {
+    if (err){
+      console.log(err)
+    }
+    console.log(restaurants)
     res.send(restaurants);
   });
 };
 
 export const addRestaurants = async (req, res) => {
+  const restaurantCollection = await connectToMongo();
   let newRestaurant = req.body;
   await restaurantCollection.insertOne(newRestaurant);
   res.send({ 'Restaurant was added': true });
 };
 
 export const addLike = async  (req, res) => {
-  const { likes } = req.body;
+  const restaurantCollection = await connectToMongo();
+  // const { likes } = req.body;
   // restaurantCollection
   //   .findOneAndUpdate(
   //     { _id: new ObjectId(req.params.id) },
@@ -33,7 +42,7 @@ export const addLike = async  (req, res) => {
 
     const upddateResults = await restaurantCollection.findOneAndUpdate(
       { _id: new ObjectId(req.params.id) },
-      { $inc: { likes: likes } })
+      { $inc: { likes: 1 } })
 
     const AllRestauntsNowWithCoolLikes =  await restaurantCollection.find({_id: req.params.id}).toArray()
     res.status(201).json(AllRestauntsNowWithCoolLikes );
